c7f0725ae7082c618568a754dfb72806fc7cad1b
Sanya <sanyakumari383@gmail.com>
2026-04-03 17:50:34 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int>result;
+      for(int i=0;i<nums.size();i++){
+        int product =1;
+  
+        for(int j=0;j<nums.size();j++){
+            if(i!=j){
+            product =product  * nums[j];
+        }
+        }
+        result.push_back(product);
+      }
+     return result;
+    }
+};
